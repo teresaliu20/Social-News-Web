d75258e612c12fd6d7aee91139a340a46ff6a38d
Teresa Liu <teresali@usc.edu>
2019-02-28 22:33:05 -0800

    redoing sass

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from 'styles/Header.scss'
+
+const Header = () => (
+  <header className='header'>
+    <Link href='/'>
+      <h3>Paper</h3>
+    </Link>
+    <menu className='menu'>
+      <Link href='/'>
+        <div className='menu-item'>
+          <a>Home</a>
+        </div>
+      </Link>
+    </menu>
+    <style jsx>{styles}</style>
+  </header>
+)
+
+export default Header

aa20c864976f11594d223af18529c951f4a6a7e7
Teresa Liu <teresali@usc.edu>
2019-03-05 21:46:48 -0800

    testing

--- src/components/LinksSection.js
+++ src/components/LinksSection.js
@@ -2,18 +2,35 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import styles from 'styles/LinksSection.scss';
+import MicrolinkCard from '@microlink/react';
+import validURL from '../helpers/validUrlHelper';
 
 const LinksSection = ({ links }) => (
   <div className="links-section">
     {
-    links && links.length ? links.map((link) => (
-      <div className="link">
-        <p className="text-sans-serif">
+    links && links.length ? links.map((link) => {
+      console.log(validURL(link.url));
+      return (
+        <div className="link" key={link.id}>
+
+          {
+          validURL(link.url)
+            ? (
+              <MicrolinkCard
+                link={link.url}
+              />
+            )
+            : (
+              <p className="text-sans-serif">
+
 &bull;&nbsp;&nbsp;
-          <a href={link.url}>{link.url}</a>
-        </p>
-      </div>
-    ))
+                <a href={link.url}>{link.url}</a>
+              </p>
+            )}
+
+        </div>
+      );
+    })
       : <div>No links right now! </div>
     }
     <style jsx>{styles}</style>
